6234f830c2d15a8f9b036fc5df2997c7ee950326
jjiiaarr <jiiarr.glorioso@gmail.com>
2023-08-26 13:24:05 +0800

    done building movie list component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import React from "react";
 
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
+import MovieList from "./components/Navbar/MovieList/MovieList";
 
 const App = () => {
   return (
     <div className="app">
       <Navbar />
-      <main>Main Content</main>
+      <MovieList />
     </div>
   );
 };

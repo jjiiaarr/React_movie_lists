efe382335037ebf481d7942c00e343630fd3a67c
jjiiaarr <jiiarr.glorioso@gmail.com>
2023-08-26 13:52:58 +0800

    fix code bugs

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -30,22 +30,8 @@ const MovieList = () => {
           </select>
         </div>
       </header>
-
       <div className="movie_cards">
         <MovieCard />
-        <div className="movie_details">
-          <h3 className="movie_details_heading">Movie Name</h3>
-          <div className="movie_date_rate">
-            <p>10-20-2020</p>
-            <p>
-              8.0 <img src={Star} alt="rating icon" className="card_emoji" />
-            </p>
-          </div>
-          <p className="movie_description">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere
-            pariatur magnam porro enim maiores libero.
-          </p>
-        </div>
       </div>
     </section>
   );
